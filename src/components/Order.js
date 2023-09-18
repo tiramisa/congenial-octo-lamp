c483ee1c4b5b2103e3457844f7c1472927d6707a
tiramisa <miller19988@gmail.com>
2023-09-18 01:59:41 +0100

    alt

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,15 @@
-import React, { Component } from "react";
-import { FaTrash } from "react-icons/fa";
+import React, { Component } from 'react';
+import { FaTrash } from 'react-icons/fa';
 export class Order extends Component {
   render() {
     return (
       <div className="item">
-        <img width={360} height={350} src={"/img/" + this.props.item.img} />
+        <img
+          width={360}
+          height={350}
+          src={'/img/' + this.props.item.img}
+          alt=""
+        />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.price}$</p>
         <FaTrash
