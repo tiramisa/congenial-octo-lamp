65dcc0d915c598298e5991adeb5a7442dd469308
tiramisa <miller19988@gmail.com>
2023-09-18 02:40:14 +0200

    a

---
+++ src/src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import Order from "./Order";
+
+const showOrders = (props) => {
+  return (
+    <div>
+      {props.orders.map((el) => (
+        <Order onDelete={props.onDelete} key={el.id} item={el} />
+      ))}
+    </div>
+  );
+};
+const showNothing = () => {
+  return (
+    <div className="empty">
+      <h2>Товаров нет</h2>
+    </div>
+  );
+};
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false);
+  return (
+    <header>
+      <div>
+        <span className="logo">House Staff</span>
+        <ul className="nav">
+          <li>Про нас</li>
+          <li>Контакты</li>
+          <li>Кабинет</li>
+        </ul>
+        <FaShoppingCart
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-card-button ${cartOpen && "active"}`}
+        />
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+          </div>
+        )}
+      </div>
+      <div className="presentation"></div>
+    </header>
+  );
+}
